fecdf91ad46468b7f827c4f0a7b54f22a1ba5635
{Bhanu} <{bhanuseshasai95@gmail.com}>
2025-08-16 11:52:27 +0530

    Fixed album.css import path

--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import folders from "../data/folders";
 import SongCard from "../components/SongCard";
-import "../styles/album.css";
+import "../Styles/album.css";
 import { MusicContext } from "../context/MusicContext";
 
 export default function AlbumPage() {
